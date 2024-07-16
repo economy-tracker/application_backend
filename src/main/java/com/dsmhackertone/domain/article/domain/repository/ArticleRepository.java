29f13df663f7598e69be9333c47e41844b0a9a5a
khyunwook <khyunwook0707@gmail.com>
2024-07-16 19:37:35 +0900

    ref :: 쿼리 변경

--- src/main/java/com/dsmhackertone/domain/article/domain/repository/ArticleRepository.java
+++ src/main/java/com/dsmhackertone/domain/article/domain/repository/ArticleRepository.java
@@ -2,10 +2,15 @@ package com.dsmhackertone.domain.article.domain.repository;
 
 import com.dsmhackertone.domain.article.domain.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface ArticleRepository extends JpaRepository<Article, UUID> {
+
+    @Query("SELECT a FROM Article a ORDER BY a.pubDate DESC")
+    List<Article> findAllOrderByPubDate();
 }
