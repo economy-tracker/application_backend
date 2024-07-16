67adf4fb5c9fa7c5b019ca2ebc4153cadc51c0c3
khyunwook <khyunwook0707@gmail.com>
2024-07-16 15:27:00 +0900

    init :: project

---
+++ src/main/java/com/dsmhackertone/DsmhackertoneApplication.java
@@ -0,0 +1,13 @@
+package com.dsmhackertone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsmhackertoneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DsmhackertoneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dsmhackertone/DsmhackertoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsmhackertone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmhackertoneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
