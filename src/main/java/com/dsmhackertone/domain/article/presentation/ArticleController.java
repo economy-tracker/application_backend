06d204c808a91e47a58c3a3eb0f8b03b4ef545c9
khyunwook <khyunwook0707@gmail.com>
2024-07-17 10:12:51 +0900

    feat :: GetArticlePercent 엔드포인트 매핑

--- src/main/java/com/dsmhackertone/domain/article/presentation/ArticleController.java
+++ src/main/java/com/dsmhackertone/domain/article/presentation/ArticleController.java
@@ -2,12 +2,16 @@ package com.dsmhackertone.domain.article.presentation;
 
 import com.dsmhackertone.domain.article.presentation.req.GetArticleListRequest;
 import com.dsmhackertone.domain.article.presentation.res.GetArticleListResponse;
+import com.dsmhackertone.domain.article.presentation.res.GetArticlePercentResponse;
 import com.dsmhackertone.domain.article.service.GetArticleListService;
+import com.dsmhackertone.domain.article.service.GetArticlePercentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/articles")
@@ -19,6 +23,10 @@ public class ArticleController {
     @GetMapping
     public GetArticleListResponse getArticle(@RequestBody GetArticleListRequest request) {
         return getArticleListService.execute(request);
+
+    @GetMapping("/chart")
+    public List<GetArticlePercentResponse> getArticlePercent() {
+        return getArticlePercentService.execute();
     }
 
 }
